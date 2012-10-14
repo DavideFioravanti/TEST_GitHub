edf54b99df9ab25239efb066bf0e5ac5ed25dbb3
Davide Fioravanti <pantanastyle@gmail.com>
2012-10-14 14:27:49 +0200

    Aggiunta animazione loopata!

--- src/com/example/test_github/MainActivity.java
+++ src/com/example/test_github/MainActivity.java
@@ -18,8 +18,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         
-        TextView textView1 = (TextView)findViewById(R.id.textView);
-        textView1.startAnimation(a);
+        final TextView textView = (TextView)findViewById(R.id.textView);
+        textView.startAnimation(ani_start);
+        textView.postDelayed(new Runnable() {
+            
+            public void run() {
+            	textView.startAnimation(ani_loop);
+            }
+        }, 1000);
+        
         
 
     }
@@ -30,21 +37,27 @@ public class MainActivity extends Activity {
         return true;
     }
     
+
+
 	static int pivotX;
 	static int pivotY;
 	static Camera camera =  new Camera();
 	
 	
-    Animation a = new Animation()
+    Animation ani_start = new Animation()
     {
 
-    @Override
+
+
+	@Override
     public void initialize(int width, int height, int parentWidth, int parentHeight){
     	super.initialize(width, height, parentWidth, parentHeight);
     	pivotX=width/2;
     	pivotX=width/2;
     	setDuration(1000L);
     	setFillAfter(true);
+
+    	
     	
     }
     
@@ -52,7 +65,7 @@ public class MainActivity extends Activity {
     protected void applyTransformation (float interpolatedTime, Transformation t){
     	Matrix matrix = t.getMatrix();
     	camera.save();
-    	camera.rotateX(interpolatedTime*60);
+    	camera.rotateX(interpolatedTime*-60);
     	camera.getMatrix(matrix);
     	
 		matrix.preTranslate(-pivotX, -pivotY);
@@ -61,4 +74,39 @@ public class MainActivity extends Activity {
     }
     
     };
+	
+    Animation ani_loop = new Animation()
+    {
+
+
+
+	@Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight){
+    	super.initialize(width, height, parentWidth, parentHeight);
+    	pivotX=width/2;
+    	pivotX=width/2;
+    	setDuration(1000L);
+    	setFillAfter(true);
+    	setRepeatMode(REVERSE);
+    	setRepeatCount(INFINITE);
+    	
+    	
+    }
+    
+    
+    protected void applyTransformation (float interpolatedTime, Transformation t){
+    	Matrix matrix = t.getMatrix();
+    	camera.save();
+    	float rotazione = interpolatedTime-0.5f;
+    	camera.rotateX( (rotazione*120));
+    	camera.getMatrix(matrix);
+    	
+		matrix.preTranslate(-pivotX, -pivotY);
+    	matrix.postTranslate(pivotX, pivotY);
+    	camera.restore();
+    }
+    
+    };
+    
+   
 }
