a3ba145c76ce2af171dabb8e71e258a3c1a0f22e
Davide Fioravanti <pantanastyle@gmail.com>
2012-10-14 13:01:33 +0200

    Aggiunta animazione Basilare

--- src/com/example/test_github/MainActivity.java
+++ src/com/example/test_github/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.test_github;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,12 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        TextView textView1 = (TextView)findViewById(R.id.textView);
+        textView1.startAnimation(a);
+        
+
     }
 
     @Override
@@ -17,4 +29,36 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+	static int pivotX;
+	static int pivotY;
+	static Camera camera =  new Camera();
+	
+	
+    Animation a = new Animation()
+    {
+
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight){
+    	super.initialize(width, height, parentWidth, parentHeight);
+    	pivotX=width/2;
+    	pivotX=width/2;
+    	setDuration(1000L);
+    	setFillAfter(true);
+    	
+    }
+    
+    
+    protected void applyTransformation (float interpolatedTime, Transformation t){
+    	Matrix matrix = t.getMatrix();
+    	camera.save();
+    	camera.rotateX(interpolatedTime*60);
+    	camera.getMatrix(matrix);
+    	
+		matrix.preTranslate(-pivotX, -pivotY);
+    	matrix.postTranslate(pivotX, pivotY);
+    	camera.restore();
+    }
+    
+    };
 }
